8466a37dcdb956a1c7e520dc695dd8aaefbd77cc
Dylan Turner <dylantdmt@gmail.com>
2018-12-05 18:50:47 -0500

    Fixed tabs issue/Converted to spaces (main.asm)

--- src/main.asm
+++ src/main.asm
@@ -20,23 +20,23 @@
     
     radix   dec
     
-KEY_ROW	    equ	    0		    ; Which byte to of key_buff to store row data in
-KEY_COL	    equ	    1		    ; Which byte to store row and shift data in
+KEY_ROW	    equ	    0                   ; Which byte to of key_buff to store row data in
+KEY_COL	    equ	    1                   ; Which byte to store row and shift data in
     
-    udata   0x20		    ; Register file locations for system variables
+    udata   0x20                        ; Register file locations for system variables
 
 key_buff    res	    2
-i	    res	    1		    ; small loop var
-k	    res	    1		    ; outer loop var
-m	    res	    2		    ; loop var for large numbers
-n	    res	    2		    ; outer loop var for big numbers
-t	    res	    1		    ; Second working directory
+i	    res	    1                       ; small loop var
+k	    res	    1                       ; outer loop var
+m	    res	    2                       ; loop var for large numbers
+n	    res	    2                       ; outer loop var for big numbers
+t	    res	    1                       ; Second working directory
 
-RES_VECT    CODE    0x0000	    ; Processor reset vector
+RES_VECT    CODE    0x0000              ; Processor reset vector
     goto    start
     
-INT_VECT  CODE	    0x0004	    ; interrupt vector
-    goto    isr			    ; go to interrupt service routine
+INT_VECT  CODE	    0x0004              ; interrupt vector
+    goto    isr                         ; go to interrupt service routine
     
 MAIN_PROG   CODE
 start
@@ -50,13 +50,13 @@ start
 ; Update: Dylan - added key_update
 isr
     movlw   0x00
-    movwf   INTCON		    ; Disable other interrupts
+    movwf   INTCON                      ; Disable other interrupts
     
     call    key_update
     
-    movlw   b'10001000'		    ; Enable keyboard interrupt
+    movlw   b'10001000'                 ; Enable keyboard interrupt
     movwf   INTCON
-    return			    ; Highly suspicious. May not worlk!
+    return                              ; Highly suspicious. May not worlk!
     
 ; Description:
 ; This is the main program loop where all other functions are called.
@@ -73,28 +73,29 @@ sys_loop
 ; Update: Dylan - Implemented double function
 key_update
     ; See if shift is pressed
-    btfsc   PORTD, 7		    ; Is the button pressed? (active low)
-    bsf	    key_buff + KEY_COL, 7   ; If so, set the column register to store shift @ b7
+    btfsc   PORTD, 7                    ; Is the button pressed? (active low)
+    bsf	    key_buff + KEY_COL, 7       ; If so, set the column register to store shift @ b7
     
-    movlw   0x77		    ; 0b0 1111110
+    movlw   0x77                        ; 0b0 1111110
     movwf   k
     call check_each_column
     return
+    
 ; Description:
 ; Small loop to iterate through each column of the keyboard
 ; Update: Dylan - Created
 check_each_column
     ; Check column 0 for each row
-    movf    k			    ; move the mask into k
+    movf    k                           ; move the mask into k
     movwf   PORTD
-    movlw   0			    ; Set i to 1 to start (it will be shifted not incremented)
+    movlw   0                           ; Set i to 1 to start (it will be shifted not incremented)
     movwf   i
     call check_each_row
     
-    btfsc   k, 6		    ; return right before you would shift to the 7th bit (bc we don't want to do that!)
+    btfsc   k, 6                        ; return right before you would shift to the 7th bit (bc we don't want to do that!)
     return
     
-    rlf	    k			    ; Shift mask (Don't forget to re-add 1 to the end)
+    rlf	    k                           ; Shift mask (Don't forget to re-add 1 to the end)
     incf
     goto    check_each_column
     
@@ -104,35 +105,35 @@ check_each_column
 check_each_row
     ; Button is active low, so first read in C and flip it to be active high
     movf    PORTC, w
-    comf    w			    ; Get 2's complement of w (invert and add 
+    comf    w                           ; Get 2's complement of w (invert and add 
     incf    w
     
     ; Now check if current w is correct
-    andwf   i, w		    ; Will check only current bit
-    btfsc   STATUS, Z		    ; Check if pressed (pressed => != 0 => Z = 0)
+    andwf   i, w                        ; Will check only current bit
+    btfsc   STATUS, Z                   ; Check if pressed (pressed => != 0 => Z = 0)
     call    row_check_set
     btfss   STATUS, Z
     call    row_check_clear
     
-    rlf	    i			    ; Shift i to next bit
-    btfss   i, 7		    ; When we get to the 8th row, i is 7th bit, stop
+    rlf	    i                           ; Shift i to next bit
+    btfss   i, 7                        ; When we get to the 8th row, i is 7th bit, stop
     return
     goto    check_each_row
 row_check_set
-    movf    k, w		    ; Get the current bit mask for Port D
-    comf    w			    ; Invert to get something like 0b10010000 instead of 0b01101111
-    bsf	    w, 7		    ; Set most significant bit to off so we don't affect it (it's shift remember!)
-    iorwf   key_buff + KEY_COL, f   ; Or the current columns with the mask, 0b00010000 for example
+    movf    k, w                        ; Get the current bit mask for Port D
+    comf    w                           ; Invert to get something like 0b10010000 instead of 0b01101111
+    bsf	    w, 7                        ; Set most significant bit to off so we don't affect it (it's shift remember!)
+    iorwf   key_buff + KEY_COL, f       ; Or the current columns with the mask, 0b00010000 for example
     movf    i, w
-    iorwf   key_buff + KEY_ROW, f   ; Mask, set the hit bit only
+    iorwf   key_buff + KEY_ROW, f       ; Mask, set the hit bit only
     return
 row_check_clear
-    movf    k, w		    ; Get the current bit mask for Port D
-    bsf	    w, 7		    ; Set most significant bit to on so we don't affect it (it's shift remember!)
-    andwf   key_buff + KEY_COL, f   ; And the current columns with the mask, 0b11101111 for example
+    movf    k, w                        ; Get the current bit mask for Port D
+    bsf	    w, 7                        ; Set most significant bit to on so we don't affect it (it's shift remember!)
+    andwf   key_buff + KEY_COL, f       ; And the current columns with the mask, 0b11101111 for example
     movf    i, w
     comf    w
-    andwf   key_buff + KEY_ROW, f   ; Mask, clear the hit bit only
+    andwf   key_buff + KEY_ROW, f       ; Mask, clear the hit bit only
     return
 
 ; Description:
@@ -154,13 +155,13 @@ key_init
     
     ; Clear initial output pins (by setting high)
     banksel PORTC
-    movlw   0x7F		    ; Don't set last bit! (MSB)
+    movlw   0x7F                        ; Don't set last bit! (MSB)
     movwf   PORTD
     clrf    PORTC
     
     ; Setup the keyboard interrupt bit
     movlw   b'10001000'
-    movwf   INTCON		    ; Enable B0 interrupt
+    movwf   INTCON                      ; Enable B0 interrupt
     
     return
 
