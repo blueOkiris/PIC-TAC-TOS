58a64716a8046b88bc7281671e59ce5f7e38f0df
Nicklas Carpenter <carpenter.nicklas@gmail.com>
2019-01-21 02:03:36 -0500

    Updated PM_Read_Write_Test.
    The example should now have better commenting and generally better code quality.

--- examples/PM_read_write_test.X/main.asm
+++ examples/PM_read_write_test.X/main.asm
@@ -1,6 +1,7 @@
 ; ***** Program Memory (EEPROM) Read-Write Test ****
 ; Nicklas Carpenter
-; December 6, 2018
+; Created: December 6, 2018
+; Updated: January 10, 2019
 ;
 ; This is a simple program that attempts to read from and write to the program
 ; memory of the PIC16F887. It's purpose is to test the extent and ability of
@@ -17,53 +18,71 @@
 
 
 TEST_LITERAL	equ 0xFF    ; Test literal so movlw command is clearly visible
-TEST_INSTR_H	equ 0x00    ; High byte of instruction to read from
-TEST_INSTR_L	equ 0x08    ; Low byte of instruction to read from
+INST_ADDR_H	equ 0x00    ; High byte of instruction to read from / write to
+INST_ADDR_L	equ 0x08    ; Low byte of instruction to read from / write to
 
-; The instruction we are writing is "movlw 0x00".
-WRITE_INSTR_H	equ 0x30    ; High byte of instruction to write  
-WRITE_INSTR_L	equ 0x0F    ; Low byte of instruction to write     
+END_ADDR_L  equ	0x0F	; The last memory address to write to
 
-bank_0	udata   0x20
-inst_rslt   res 1	; The result of the operation we read and change
+; The instruction we are writing is "movlw 0x0F".
+INSTR_H	    equ 0x30    ; High byte of instruction to write  
+INSTR_L	    equ 0x0F    ; Low byte of instruction to write
+     
     
 ; We store instructions in bank 3 to prevent continuous switching during write
-bank_3	udata   0x110		 
-cur_addr    res 1	; The offset from the instruction we are writing.
-inst_l res 1   ; The low byte of the instruction read
-inst_h  res 1   ; The high byte of the instruction read 
+bank_2	    udata   0x110		 
+inst_l	    res 1   ; The low byte of the instruction read
+inst_h	    res 1   ; The high byte of the instruction read
+inst_rslt   res 1	; The result of the operation we read and change 
     
-
+; I don't know why I specify this, since nine out of ten times I use hexadecimal
+; anyway. I guess I like to be explicit with my hexadecimal (i.e. including the
+; '0x' prefix), but I would rather not include a prefix if and when I use 
+; decimal.
     radix dec
-RST_VECT    code    0x0000
-    goto    start
 
-MAIN_PROG   code    0x0005
+MAIN_PROG    code    0x0000
 start
+    ; Read the line as it's initially written.
+    call    pm_read
+    ; Move the original literal to the specified register
+    call    test_sequence
+    ; Modify 'test_sequence to move a new, different literal to the specified
+    ; register
     call    pm_write
+    ; Read the same lines. Basically, we're verifying our changes to program 
+    ; memory were actually made
+    call    pm_read
+    ; Run the updated function. Icing on the cake.
+    call    test_sequence
 end_loop
     goto    end_loop
 
+; Jump so we write to the correct location in program memory. We leave some
+; empyt spaces, but that shouldn't matter for our purposes. Additionally, we 
+; overwrite the interrupt vector. This is also okay, since this program 
+; doesn't use any interrupts. 
+USER_SPACE  code    0x0007
 test_sequence
     clrf    inst_rslt
     ; The next instruction is addressed at 0x008 so we can write to it.
-    ; In general it seems you can only write to addresses with zeroes in the 
-    ; least significant bit.
     movlw   TEST_LITERAL    ; This is the instruction we want to read or change.
     movwf   inst_rslt
     return
 
-;   ****  pm_read ****
-;   Read the instruction we wrote to the specified address 
-;   We skip a few addresses between test_sequence and this function to ensure
-;   this function is not overwriten by pm_write
-SECT_2	code	0x0010
+; We start the definition for our other fucnction starting at the next write
+; block (program memory address 16). Every write to program memory has to 
+; contain exactly 8 instructions, and each write has to start at an address
+; whose least three significant bytes are 0 (i.e. 0bxxxxxxxxxxxxx000). 
+KERNAL_SPACE	code	0x0010
+
+; ****  pm_read ****
+; Read the instruction we wrote to the specified address
 pm_read	    	
     ; **** Initiating the read from program memory
     banksel EEADR   ; Select bank to specify where to read from in PM
-    movlw   TEST_INSTR_H
+    movlw   INST_ADDR_H
     movwf   EEADRH  ; High byte of program memory to read
-    movlw   TEST_INSTR_L
+    movlw   INST_ADDR_L
     movwf   EEADR   ; Low byte or program memory to read
     banksel EECON1  ; Select bank to specify when to read from PM
     bsf	    EECON1, EEPGD   ; Read from program memory as opposed to data memory
@@ -90,24 +109,33 @@ pm_read
     return
 
 ; **** pm_write ****
-; Writes an instruction to program memory
+; Writes an instruction to program memory. This can be made more efficient when
+; generalized. For now, it serves to demonstrate the basic concept.
 pm_write
-    ; **** Load the instruction into data and select its address
+    ; **** Load the test instruction into data and select its address.
     banksel EEADR
-    movlw   TEST_INSTR_H    
+    movlw   INST_ADDR_H    
     movwf   EEADRH	    ; Load high byte of initial instruction address.
-    movlw   TEST_INSTR_L
-    movwf   EEADR	    ; Load low byte of initial instruction address
-    movlw   WRITE_INSTR_H  
+    movlw   INST_ADDR_L
+    movwf   EEADR
+    movlw   INSTR_H  
     movwf   EEDATH	    ; Load high byte of instruction into buffer
-    movlw   WRITE_INSTR_L   
+    movlw   INSTR_L   
     movwf   EEDAT	    ; Load low byte of instruction into buffer
-    movlw   TEST_INSTR_L
-    movwf   cur_addr	    
-    incf    cur_addr, f	    ; Set offset to one so we can write to next address
+    movlw   INST_ADDR_L
 
-; **** Write EEDAT and EEDATH
+; **** Write to EEDAT and EEDATH
 write
+    ; Check to ensure  we are not writing out of bounds: see if the address to
+    ; be loaded into EADDR is past the end address. If we are in bounds, 
+    ; initiate the write, otherwise we are done.
+    bcf	    STATUS, RP0
+    movlw   END_ADDR_L + 1 
+    subwf   EEADR, w
+    btfsc   STATUS, Z
+    return
+    
+    
     ; Preform the write operation
     banksel EECON1	    ; Select the program memory read configureation
     bsf	    EECON1, EEPGD   ; Write to program data instead of EEPROM
@@ -123,38 +151,29 @@ write
     ; It takes two instruction cycles to write to memory
     nop
     nop
+
     bcf	    EECON1, WREN    ; Disable writing to program memory (for safety)
     
+    ; ** Initialize the next write cycle
     banksel EEADR
-    movlw   0x00    	    
+    movlw   INST_ADDR_H    	    
     movwf   EEADRH
-    movf    cur_addr, w
-    movwf   EEADR
-    incf    cur_addr
-    banksel EECON1  ; Select bank to specify when to read from PM
+    incf    EEADR   ; Move to the next instruction
+
+    banksel EECON1	    ; Select bank to specify when to read from PM
     bsf	    EECON1, EEPGD   ; Read from program memory as opposed to data memory
-    bsf	    EECON1, RD	; Initiate a read from program memory
+    bsf	    EECON1, RD	    ; Initiate a read from program memory
     
-    ; It takes two instruction cycles to read from program memory.
-    ; We can do something during the first instruction, but the second
-    ; instuction is ignored. We have nothing better to do here, so we'll make
-    ; both instructions No-ops.
+    ; It takes two instruction cycles to read from program memory. After 
+    ; initiating a write, the two following instructions must be nops. 
+    ; Ostensibly, the instruction immediately following the read initiation
+    ; should execute, but this did not seem to be the case on testing. Note that
+    ; in the simulator the nops are not needed: the two instructions following
+    ; the read initiation appear to be executed normally.
     nop
     nop
     
-    banksel cur_addr
-    ; Check to see if we should stop writing to the buffer
-    movlw   0x11 ; This is one past the address where we want to stop
-		 ; cur_addr is incremented but not used for the last value.
-    subwf   cur_addr,w
-    btfss   STATUS,Z
-    goto    write
-    
-    ; Test our changes.
-    call    pm_read
-    banksel inst_rslt
-    call    test_sequence
-    return
+    goto write
     
     end
 
